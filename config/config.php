9e375ff674639b9a5fb4332d676f64a8603ae21d
Ashish Tripathi <tripathi.ashish333@yahoo.com>
2025-12-09 17:29:29 +0530

    Remove deprecated testing files and update error reporting configuration

--- config/config.php
+++ config/config.php
@@ -48,7 +48,8 @@ define('TERMS_URL', getenv('TERMS_URL') ?: 'https://shivarya.github.io/split-cas
 // Timezone
 date_default_timezone_set('Asia/Kolkata');
 
-// Error reporting (disable in production)
+// Error reporting (keep logs on; display off). Log to project file for easier debugging.
 error_reporting(E_ALL);
 ini_set('display_errors', '0');
 ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/../php_errors.log');
